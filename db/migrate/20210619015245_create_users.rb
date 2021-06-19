4a8240a5d8f7c3174fb69cbeff7a203840b11b1d
prahal <vprahaladhan@gmail.com>
2021-06-19 21:12:40 +1000

    added users controller, views, and model

--- db/migrate/20210619015245_create_users.rb
+++ db/migrate/20210619015245_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
     create_table :users do |t|
       t.string :name
       t.string :email
+      t.string :password_digest
       t.string :provider
       t.string :uid
       t.string :image
